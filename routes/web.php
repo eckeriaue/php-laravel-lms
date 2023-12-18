9575429ad220d18d01905e3c68e2ef2d150f774d
L <eckeriaue@gmail.com>
2023-12-19 00:34:17 +0300

    auth fixes

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    if (Auth::check()) {
+        return view('dashboard');
+    }
+    return redirect('login');
 });
 
 Route::get('/dashboard', function () {
