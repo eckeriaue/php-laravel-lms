97ddc43c421c0b9b2c9af9051c60bd8ad6d77263
root <root@zmqmaxgwdh.local>
2023-12-18 12:38:16 +0000

    add login page

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function (Request $req) {
     return view('home');
+});
+
+
+Route::get('/login', function (Request $req) {
+    return view('login');
 });
