d9652789673c7f1160f92cad7ffda1166396d9dd
L <eckeriaue@gmail.com>
2023-12-18 22:19:34 +0300

    add window component

---
+++ app/View/Components/Window.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\View\Components;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Window extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return <<<'HTML'
+            @props([
+                'title' => 'Document',
+            ])
+
+            <!DOCTYPE html>
+            <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>{{ $title }} </title>
+                @vite(['resources/js/app.js'])
+            </head>
+            <body>
+                {{ $slot }}
+            </body>
+            </html>
+        HTML;
+    }
+}
+
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,20 +1,11 @@
+<x-window title="sdfsd">
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    @vite(['resources/js/app.js'])
-</head>
-<body>
-<div x-data="{ expanded: false }">
+    <div x-data="{ expanded: false }">
         <button type="button"  @click="expanded = ! expanded">
             <span x-show="! expanded">Show post content...</span>
             <span x-show="expanded">Hide post content...</span>
         </button>
- 
     </div>
     <a href="/login"> to login </a>
-</body>
-</html>
+
+</x-window>
