a03ed8eb889aeee5aea3264e4a20676311c4d4a7
L <eckeriaue@gmail.com>
2023-12-23 04:10:03 +0300

    fix toggle theme

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
 
 
-if (localStorage.getItem('darkMode_on') === 'true') document.body.classList.add('dark')
+if (localStorage.getItem('darkMode_on') === 'true') document.documentElement.classList.add('dark')
 
 window.Alpine = Alpine
 Alpine.plugin(persist)
@@ -12,7 +12,7 @@ Alpine.store('darkMode', {
   on: Alpine.$persist(false).as('darkMode_on'),
   toggle() {
     const enable = this.on = !this.on
-    const root = document.body.classList
+    const root = document.documentElement.classList
     if (enable) root.add('dark')
     else root.remove('dark')
     return enable 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" 
+        :class="{'dark': localStorage.getItem('darkMode_on') === 'true'}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -14,7 +15,8 @@
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body :class="{'dark': localStorage.getItem('darkMode_on') === 'true'}" class="font-sans antialiased text-gray-800 dark:text-gray-200">
+    <body
+        class="font-sans antialiased text-gray-800 dark:text-gray-200">
         <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
             @include('layouts.navigation')
 
