11f167239ee497d57642198e4a67a86156cad201
L <eckeriaue@gmail.com>
2023-12-23 04:44:35 +0300

    set theme in cookie

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,19 +2,34 @@ import './bootstrap'
 import Alpine from 'alpinejs'
 import persist from '@alpinejs/persist'
 
-
-if (localStorage.getItem('darkMode_on') === 'true') document.documentElement.classList.add('dark')
+const ENABLE_COOKIE_STORE = 'cookieStore' in globalThis
 
 window.Alpine = Alpine
 Alpine.plugin(persist)
+
+if(!ENABLE_COOKIE_STORE) {
+  const theme = localStorage.getItem('theme')
+  const root = document.documentElement.classList
+  if (theme && !root.contains(theme)) {
+    root.add(theme)
+  }
+}
 
 Alpine.store('darkMode', {
   on: Alpine.$persist(false).as('darkMode_on'),
   toggle() {
     const enable = this.on = !this.on
     const root = document.documentElement.classList
-    if (enable) root.add('dark')
-    else root.remove('dark')
+    if (enable) {
+      if (ENABLE_COOKIE_STORE) cookieStore.set('theme', 'dark')
+      localStorage.setItem('theme', 'dark')
+      root.add('dark')
+    }
+    else {
+      if (ENABLE_COOKIE_STORE) cookieStore.set('theme', '')
+      localStorage.setItem('theme', '')
+      root.remove('dark')
+    }
     return enable 
   }
 })
